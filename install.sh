2faf0f670c974580772178dd5f46e562a93c2fe4
Filippo Errico <filipp.rrc@gmail.com>
2022-08-12 12:21:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ cmsrel CMSSW_10_6_26
 cd CMSSW_10_6_26/src/
 cmsenv
 git cms-init
-git clone git@github.com:angzaza/hcc_v2.git
+git clone git@github.com:errico/Hcc_v2.git
+                                                        git clone git@github.com:angzaza/hcc_v2.git
 git cms-merge-topic bonanomi:ElectronsMVA_UL
 #git cms-merge-topic asculac:Electron_XGBoost_MVA_16UL_17UL
 git cms-addpkg GeneratorInterface/RivetInterface
